27b11518acd3ce515ec11e246819fd647748b2de
omeshapasan2 <omesha.20232170@iit.ac.lk>
2025-06-20 11:13:26 +0530

    Data List Added

---
+++ src/data/hotels.js
@@ -0,0 +1,35 @@
+const hotels = [
+    {
+        id: 1,
+        name: "The Phoenix Hotel",
+        category: ["Popular", "Modern"],
+        image: "",
+        rating: 4.8,
+        price: 125,
+        discountedPrice: null,
+        location: "Sleman, DIY",
+        isFavorite: true
+    },
+    {
+        id: 2,
+        name: "Aston White Coral",
+        category: ["Popular", "Beach"],
+        image: "",
+        rating: 4.8,
+        price: 130,
+        discountedPrice: null,
+        location: "Sleman, DIY",
+        isFavorite: false
+    },
+    {
+        id: 3,
+        name: "The Phoenix Hotel",
+        category: ["Popular", "Modern"],
+        image: "",
+        rating: 4.8,
+        price: 225,
+        discountedPrice: 200,
+        location: "Sleman, DIY",
+        isFavorite: true
+    }
+];
