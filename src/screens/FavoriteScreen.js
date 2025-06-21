46d30c49f5a6be89d37d8d2cf17d432a0f2909e7
omeshapasan2 <omesha.20232170@iit.ac.lk>
2025-06-21 13:22:03 +0530

    Before Tab Screen Implementation

---
+++ src/screens/FavoriteScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FavoriteScreen() {
+  return (
+    <div>
+      <h3>Favorite Screen</h3>
+    </div>
+  )
+}
+
+export default FavoriteScreen
---
+++ src/screens/MyTripScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyTripScreen() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyTripScreen
